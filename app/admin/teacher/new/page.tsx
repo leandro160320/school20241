7df629d8bd33246e2ca54d03de1c509e4c8b5a7c
leandro160320 <148804278+leandro160320@users.noreply.github.com>
2024-06-12 23:42:48 +0000

    feat: Teacher aparecendo a opção cadastrar

---
+++ app/admin/teacher/new/page.tsx
@@ -0,0 +1,86 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+
+const FormSchema = z.object({
+    name: z.string().min(2, {
+        message: "Nome precisa tem no mínimo 2 caracteres."
+    }),
+
+    email: z.string().email({ message: "Digite o email correto" }),
+})
+
+export default function SaveTeacher() {
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            name: "Leandro bianchini",
+            email: "professor@hotmail.com",
+        },
+    })
+
+    async function onSubmit(teacher: z.infer<typeof FormSchema>) {
+        const requestOption= {
+            method: "POST",
+            headers:{'Content-Type':'application/json'},
+            body: JSON.stringify(teacher)
+        }
+        const response = await fetch("https://server20241-liart.vercel.app/teachers",requestOption)
+        form.reset();
+        alert("Professor Cadastrado com Sucesso!")
+
+
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Nome</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Digite nome do Estudante" {...field} />
+                            </FormControl>
+
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>email</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Digite o email do Estudante" {...field} />
+                            </FormControl>
+
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    )
+}
